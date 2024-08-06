892ca585bc2f6ccd936bc2095899f05b5a221948
declipsonator <declipsonator@gmail.com>
2024-08-05 21:35:18 -0400

    Fix

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ script_location = os.path.dirname(os.path.abspath(sys.argv[0]))
 client = Client()
 MY_STRAVA_CLIENT_ID, MY_STRAVA_CLIENT_SECRET = open(script_location + '/client.secret').read().strip().split(',')
 
-
+MY_STRAVA_CLIENT_ID = MY_STRAVA_CLIENT_ID.strip()
+MY_STRAVA_CLIENT_SECRET = MY_STRAVA_CLIENT_SECRET.strip()
 
 # url = client.authorization_url(client_id=MY_STRAVA_CLIENT_ID, redirect_uri='http://127.0.0.1:5000/authorization',
 #                                scope=['read_all', 'profile:read_all', 'activity:read_all', 'profile:write',
@@ -33,11 +34,12 @@ def check_token():
     global access_token
     if time.time() > access_token['expires_at']:
         print('Token has expired, will refresh')
-        refresh_response = client.refresh_access_token(client_id=MY_STRAVA_CLIENT_ID,
-                                                       client_secret=MY_STRAVA_CLIENT_SECRET,
+
+        refresh_response = client.refresh_access_token(client_id=int(MY_STRAVA_CLIENT_ID),
+                                                       client_secret=str(MY_STRAVA_CLIENT_SECRET),
                                                        refresh_token=access_token['refresh_token'])
         access_token = refresh_response
-        with open(script_location +'/access_token.pickle', 'wb') as f:
+        with open(script_location + '/access_token.pickle', 'wb') as f:
             pickle.dump(refresh_response, f)
         print('Refreshed token saved to file')
         client.access_token = refresh_response['access_token']
@@ -54,7 +56,6 @@ def check_token():
 
 check_token()
 
-
 # Get all activities
 activities = client.get_activities()
 print('Activities:')
@@ -67,18 +68,20 @@ for a in activities:
     month = activity.start_date.month
     year = activity.start_date.year
     daysSince = (datetime.now().date() - activity.start_date.date()).days
-    if activity.description != "" or daysSince > 5:
+
+    if (activity.description is not None and activity.description != "") or daysSince > 5:
         break
 
     response = ollama.chat(model='llama3.1', messages=[
         {
             'role': 'user',
-            'content': 'Provide only an original quote that makes you think/reflect about yourself, the world, or life in '
-                       'general. Do'
-                       'not provide any context or explanation. Do not give any author or source. Do not surround '
-                       'with quotations.'
+            'content': 'Provide only an original quote that inspires reflection about nature, thought, or the essence of life. '
+                       'Avoid using logic-based expressions like true or false. Do not provide any context or explanation. Do not '
+                       'attribute the quote to any author or source. Do not use quotation marks.'
+
         },
     ])
     print(response["message"]["content"])
-    client.update_activity(activity.id, description=response["message"]["content"] + "\n\n-llama3.1   \n" + datetime.now()
+    client.update_activity(activity.id,
+                           description=response["message"]["content"] + "\n\n-llama3.1   \n" + datetime.now()
                            .strftime("%Y-%m-%d %H:%M:%S"))
